b7baf257b78a921d96f43c7f2be57f382f5a4bd2
guilhermetomaz <guilherme.camara@edu.senai.br>
2026-05-20 10:26:42 -0300

    feat: adiciona notas e cálculos de média

--- main.cpp
+++ main.cpp
@@ -8,30 +8,54 @@ using namespace std;
 int main() {
     SetConsoleOutputCP(65001);
 
-//Entrada - declaração das variáveis
-string nomes[20];
-int qntdAlunos;
+    string nomes[20];
+    int qntdAlunos;
+    float notas[20][5];
+    float media[20];
+    int qntdDisciplinas;
 
-//Processamento
-cout << "=== SISTEMA DE NOTAS v4.0===" << endl;
+    cout << "=== SISTEMA DE NOTAS v4.0 ===" << endl;
 
-do {
-    cout << "Quantidade de alunos (1 a 20): ";
-    cin >> qntdAlunos;
-}while (qntdAlunos < 1 || qntdAlunos > 20);
+    do {
+        cout << "Quantidade de alunos (1 a 20): ";
+        cin >> qntdAlunos;
+    } while (qntdAlunos < 1 || qntdAlunos > 20);
 
+    cin.ignore();
 
-cin.ignore();
+    for (int i = 0; i < qntdAlunos; i++) {
+        cout << "Nome do aluno " << i + 1 << ": ";
+        getline(cin, nomes[i]);
+    }
 
-for (int i = 0; i < qntdAlunos; i++){
-    cout << "Nome do aluno: " << i + 1 << ":" ;
-    getline(cin, nomes[i]);
-}
+    do {
+        cout << "Quantidade de disciplinas (1 a 5): ";
+        cin >> qntdDisciplinas;
+    } while (qntdDisciplinas < 1 || qntdDisciplinas > 5);
 
-//Saída
-cout << "\nAlunos cadastrados:" << endl;
-for (int i = 0; i < qntdAlunos; i++){
-cout << "  " << i + 1 << ". " << nomes[i] << endl;
-}
+    
+    cin.ignore(); 
+
+    for (int i = 0; i < qntdAlunos; i++) {
+        cout << "\nNotas do aluno: " << nomes[i] << endl;
+        float soma = 0;
+        for (int j = 0; j < qntdDisciplinas; j++) {
+            do {
+                cout << "  Disciplina " << j + 1 << " (0 a 10): ";
+                cin >> notas[i][j];
+                
+            } while (notas[i][j] < 0 || notas[i][j] > 10); 
+            
+            soma += notas[i][j];
+        }
+        media[i] = soma / qntdDisciplinas;
+    }
+
+    cout << "\n==============================" << endl;
+    cout << "RELATÓRIO FINAL:" << endl;
+    for (int i = 0; i < qntdAlunos; i++) {
+        cout << i + 1 << ". " << nomes[i] << " - Média: " << media[i] << endl;
+    }
+
     return 0;
 }
